37fe817f28a24116aad73427134f00f5ff94b41a
Adam Coates <ad_coates@hotmail.com>
2023-03-06 06:28:48 +0000

    still setting up

---
+++ app/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className=" bg-orange-600">
+      <p>This is the footer</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ app/components/Header/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div className=" bg-green-600">
+      <p>This is the header</p>
+    </div>
+  );
+}
+
+export default Header;
