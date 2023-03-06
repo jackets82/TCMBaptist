513950580df6931c0f0f247e17472e7d0c463692
Adam Coates <ad_coates@hotmail.com>
2023-03-06 12:35:43 +0000

    navbar

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,7 +1,29 @@
+import Image from "next/image";
+
 function Header() {
   return (
-    <div className=" bg-green-600">
-      <p>This is the header</p>
+    <div className=" bg-slate-900 py-3">
+      <div className=" max-w-6xl mx-auto flex justify-between items-center">
+        <div className=" flex">
+          <div className=" w-32">
+            <Image src="/mainLogo.png" width={500} height={500} />
+          </div>
+
+          {/* <p className=" text-sm text-gray-100">
+            Where everyone is someone and Jesus Christ is Lord
+          </p> */}
+        </div>
+        <div className=" text-white">
+          <ul className=" flex gap-3">
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Get Involved</li>
+            <li>Resources</li>
+            <li>Contact Us</li>
+            <li>Donate</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
