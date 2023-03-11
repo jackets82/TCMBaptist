95bcc693a6d385b022c64228397452293d608b18
Adam Coates <ad_coates@hotmail.com>
2023-03-11 21:29:59 +0000

    hero

--- app/components/Hero.js
+++ app/components/Hero.js
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 function Hero() {
   return (
-    <div className="relative lg:h-[1000px] block h-[800px] 2xl:h-screen">
+    <div className="relative h-screen block">
       <Image
         src="/church.jpg"
         style={{ objectFit: "cover", objectPosition: "50% 50%" }}
         fill
       />
-      <p className=" text-white text-5xl absolute top-1/2 left-1/2 -translate-x-1/2 2xl:top-1/3 text-center">
+      <p className=" text-white text-5xl absolute top-1/2 left-1/2 -translate-x-1/2 text-center">
         Welcome to TCM Lincoln
       </p>
     </div>
