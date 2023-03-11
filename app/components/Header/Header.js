83f61441eb25da44f09f108a663a7c21f4ab87d1
Adam Coates <ad_coates@hotmail.com>
2023-03-11 21:28:45 +0000

    header

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 function Header() {
   return (
-    <div className=" bg-black py-3 sticky top-0 z-10">
-      <div className=" max-w-6xl mx-auto flex justify-between items-center">
+    <div className=" bg-transparent py-3 top-0 z-10 absolute w-full">
+      <div className=" max-w-6xl mx-auto flex justify-between items-center px-10 xl:px-0">
         <div className=" flex">
-          <div className=" w-32">
+          <div className=" w-44">
             <Image src="/mainLogo.png" width={500} height={500} />
           </div>
 
@@ -14,7 +14,7 @@ function Header() {
           </p> */}
         </div>
         <div className=" text-white">
-          <ul className=" flex gap-3">
+          <ul className=" flex gap-5">
             <li>Home</li>
             <li>About Us</li>
             <li>Get Involved</li>
