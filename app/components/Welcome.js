70e6887c72f920f51096a71653eb808dd13388e1
Adam Coates <ad_coates@hotmail.com>
2023-03-11 21:30:18 +0000

    general_stuff

--- app/components/Welcome.js
+++ app/components/Welcome.js
@@ -1,16 +1,17 @@
 function Welcome() {
   return (
-    <div className=" bg-white h-[800px]">
-      <div className=" max-w-6xl mx-auto text-center pt-32 px-10">
-        <h1 className=" text-4xl underline pb-20 ">Title</h1>
-        <p className=" text-2xl pb-20">
+    <div className=" bg-white">
+      <div className=" max-w-6xl mx-auto px-10 flex flex-col content-center pt-48 pb-36 text-center">
+        <h1 className=" text-4xl pb-5 ">Title</h1>
+        <p className=" text-2xl pb-10">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
           quos quam expedita eius temporibus et ullam officiis nihil deleniti
           iure numquam explicabo dolores! Sed, quis mollitia dolor nisi
           molestias incidunt.
         </p>
-        <h1 className=" text-4xl underline pb-20">Title</h1>
-        <p className=" text-2xl">
+        <hr className="h-2 rounded w-32 mx-auto bg-gray-200 border-0 dark:bg-gray-700"></hr>
+        <h1 className="text-4xl pt-10 pb-5">Title</h1>
+        <p className="text-2xl">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
           quos quam expedita eius temporibus et ullam officiis nihil deleniti
           iure numquam explicabo dolores! Sed, quis mollitia dolor nisi
--- app/page.js
+++ app/page.js
@@ -8,24 +8,8 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className=" bg-red-600 flex-grow">
-      <Hero />
-      <Welcome />
-      <Connect />
+      {/* <Hero />
+      <Welcome /> */}
     </main>
   );
-}
-
-{
-  /* <main className=" bg-red-600 flex-grow">
-      <div className="relative lg:h-[1000px] block h-[800px] 2xl:h-screen">
-        <Image
-          src="/cross.jpg"
-          style={{ objectFit: "cover", objectPosition: "50% 0%" }}
-          fill
-        />
-        <p className=" text-white text-5xl absolute top-1/2 left-1/2 -translate-x-1/2 2xl:top-1/3 text-center">
-          Welcome to TCM Lincoln
-        </p>
-      </div>
-    </main> */
 }
