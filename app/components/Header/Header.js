b7a40e1be5d9d9a7bc4bf9dff634003a939fc91c
Adam Coates <ad_coates@hotmail.com>
2023-03-24 19:52:53 +0000

    Header menu hidden on small screens
    Former-commit-id: 2a4bb1a6b7328cecc73d91383e607cbe16c93aee

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -13,59 +13,66 @@ function Header() {
             </Link>
           </div>
         </div>
+
         <div className=" text-white">
-          <ul className=" flex gap-5">
-            <Link href="/#">
-              <li>Home</li>
-            </Link>
-            <HeaderDropDown
-              title="About Us"
-              links={[
-                { href: "/Subpages/About/Beliefs", label: "What we believe" },
-                { href: "/Subpages/About/Baptism", label: "Baptism" },
-                { href: "/Subpages/About/Membership", label: "Membership" },
-                { href: "/#", label: "Who we are" },
-                { href: "/#", label: "Mission partners" },
-                { href: "/#", label: "Our history" },
-                { href: "/#", label: "FAQs" },
-              ]}
-            />
-            <HeaderDropDown
-              title="Get Involved"
-              links={[
-                { href: "/", label: "Services" },
-                { href: "/#", label: "Extra time" },
-                { href: "/#", label: "Sunday school" },
-                { href: "/#", label: "Young adults" },
-                { href: "/", label: "Housegroups" },
-                { href: "/#", label: "Children's and youth groups" },
-                { href: "/#", label: "Christianity explored" },
-                { href: "/#", label: "Hope explored" },
-                { href: "/#", label: "Men/women" },
-                { href: "/#", label: "Music ministry team application" },
-              ]}
-            />
-            <HeaderDropDown
-              title="Resources"
-              links={[
-                { href: "/", label: "Sermons" },
-                { href: "/#", label: "Sunday school videos" },
-                { href: "/#", label: "FAQs about christianity" },
-                { href: "/#", label: "Recommendations" },
-                { href: "/", label: "Useful links" },
-                { href: "/#", label: "Members" },
-                { href: "/#", label: "Bookstall and resources center" },
-                { href: "/#", label: "Media archive" },
-              ]}
-            />
-            <HeaderDropDown
-              title="Contact us"
-              links={[{ href: "/", label: "Find us" }]}
-            />
-            <Link href="/#" className=" ">
-              <li>Donate</li>
-            </Link>
-          </ul>
+          <div className=" md:hidden">
+            <p>This is the closed menu</p>
+          </div>
+          <div className=" hidden md:inline">
+            <ul className=" flex gap-5">
+              <Link href="/#">
+                <li>Home</li>
+              </Link>
+
+              <HeaderDropDown
+                title="About Us"
+                links={[
+                  { href: "/Subpages/About/Beliefs", label: "What we believe" },
+                  { href: "/Subpages/About/Baptism", label: "Baptism" },
+                  { href: "/Subpages/About/Membership", label: "Membership" },
+                  { href: "/#", label: "Who we are" },
+                  { href: "/#", label: "Mission partners" },
+                  { href: "/#", label: "Our history" },
+                  { href: "/#", label: "FAQs" },
+                ]}
+              />
+              <HeaderDropDown
+                title="Get Involved"
+                links={[
+                  { href: "/", label: "Services" },
+                  { href: "/#", label: "Extra time" },
+                  { href: "/#", label: "Sunday school" },
+                  { href: "/#", label: "Young adults" },
+                  { href: "/", label: "Housegroups" },
+                  { href: "/#", label: "Children's and youth groups" },
+                  { href: "/#", label: "Christianity explored" },
+                  { href: "/#", label: "Hope explored" },
+                  { href: "/#", label: "Men/women" },
+                  { href: "/#", label: "Music ministry team application" },
+                ]}
+              />
+              <HeaderDropDown
+                title="Resources"
+                links={[
+                  { href: "/", label: "Sermons" },
+                  { href: "/#", label: "Sunday school videos" },
+                  { href: "/#", label: "FAQs about christianity" },
+                  { href: "/#", label: "Recommendations" },
+                  { href: "/", label: "Useful links" },
+                  { href: "/#", label: "Members" },
+                  { href: "/#", label: "Bookstall and resources center" },
+                  { href: "/#", label: "Media archive" },
+                ]}
+              />
+              <HeaderDropDown
+                title="Contact us"
+                links={[{ href: "/", label: "Find us" }]}
+              />
+              <Link href="/#" className=" ">
+                <li>Donate</li>
+              </Link>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
