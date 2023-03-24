128f3fbdf61a786e41f44eb5282939507a5801ad
Adam Coates <ad_coates@hotmail.com>
2023-03-24 12:31:03 +0000

    alt added to images
    Former-commit-id: cd63a5a08d792d48753da08d7fafbb2fb65a8ea6

--- app/components/Sundays/SundayCard.js
+++ app/components/Sundays/SundayCard.js
@@ -6,7 +6,7 @@ function SundayCard({ pic, text }) {
     <div className=" bg-gray-300 rounded overflow-hidden shadow  hover:scale-105 transition-all duration-150 hover:shadow-lg">
       <Link href="/#">
         <div className=" rounded h-36 w-56 justify-center items-center flex relative flex-wrap">
-          <Image src={pic} fill style={{ objectFit: "cover" }} />
+          <Image src={pic} fill style={{ objectFit: "cover" }} alt="" />
         </div>
         <div className=" px-2">
           <p>{text}</p>
