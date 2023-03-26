aeb36f1502f76d34f845dd102b63c714e87c6322
Adam Coates <ad_coates@hotmail.com>
2023-03-26 09:10:13 +0100

    burger dropdown position
    Former-commit-id: 019de363756a67d7cfdc11059486666f01338f23

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import BurgerDropDown from "./BurgerDropDown";
 import HeaderDropDown from "./HeaderDropDown";
 
 function Header() {
@@ -16,20 +17,24 @@ function Header() {
 
         <div className=" text-white">
           <div className=" md:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-6 h-6"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-              />
-            </svg>
+            <BurgerDropDown
+              button={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                  />
+                </svg>
+              }
+            />
           </div>
           <div className=" hidden md:inline">
             <ul className=" flex gap-5">
