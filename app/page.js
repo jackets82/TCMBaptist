9dce355ae5f4817aa29278fd5a74f02fb849f26a
Adam Coates <ad_coates@hotmail.com>
2023-04-04 15:31:30 +0100

    basic marquee started

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import Hero from "./components/Hero/Hero";
+import Marquee from "./components/Marquee/Marquee";
 import Sundays from "./components/Sundays/Sundays";
 import Welcome from "./components/Welcome";
 
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <main className=" bg-red-600 flex-grow">
       <Hero />
+      <Marquee />
       <Welcome />
       <Sundays />
     </main>
