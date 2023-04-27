2d264ffa8db644d1ff1b3ccee2c2159cbdfe67f7
Adam Coates <ad_coates@hotmail.com>
2023-04-27 20:52:30 +0100

    Loads of changes - sorry

---
+++ app/components/Front Page/whyICome/WhyICome.js
@@ -0,0 +1,20 @@
+function WhyICome() {
+  return (
+    <div className=" bg-black">
+      <p className=" text-white py-10 text-5xl text-center">Why I attend TCM</p>
+      <div className=" bg-green-700 flex justify-center h-[600px] max-w-5xl mx-auto">
+        <iframe
+          width="5000"
+          height="600"
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
+  );
+}
+
+export default WhyICome;
