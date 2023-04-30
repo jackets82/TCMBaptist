ef4ec3db3de323893a4beecb15040170fef68dcb
Adam Coates <ad_coates@hotmail.com>
2023-04-30 08:45:28 +0100

    Video settings changed, and hero video restored

--- app/components/Front Page/whyICome/WhyICome.js
+++ app/components/Front Page/whyICome/WhyICome.js
@@ -8,9 +8,9 @@ function WhyICome() {
           height="600"
           src="https://www.youtube.com/embed/dQw4w9WgXcQ"
           title="YouTube video player"
-          frameborder="0"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
+          allowFullScreen
         ></iframe>
       </div>
     </div>
