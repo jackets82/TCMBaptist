c5bf22f15823b4288d8d898d8601eda43f083d48
Adam Coates <ad_coates@hotmail.com>
2023-04-30 09:19:51 +0100

    removed apostrophes temporarily
    Former-commit-id: eff24f82280526cdfba116b79964165d290c7ac4

--- app/components/Front Page/whatson/WhatsOn.js
+++ app/components/Front Page/whatson/WhatsOn.js
@@ -6,18 +6,18 @@ function WhatsOn() {
       <h1 className=" text-4xl">What's On</h1>
       <div className=" sm:grid sm:grid-cols-2 md:grid-cols-4 flex flex-col max-w-5xl mx-auto gap-y-10">
         <WhatsOnCircle
-          title="Men's Ministry"
+          title="Mens Ministry"
           image="https://picsum.photos/200"
         />
         <WhatsOnCircle
-          title="Women's Ministry"
+          title="Womens Ministry"
           image="https://picsum.photos/200"
         />
         <WhatsOnCircle title="Extra Time" image="https://picsum.photos/200" />
         <WhatsOnCircle title="Young Adults" image="https://picsum.photos/200" />
         <WhatsOnCircle title="House Groups" image="https://picsum.photos/200" />
         <WhatsOnCircle
-          title="Children's and Youth Groups"
+          title="Childrens and Youth Groups"
           image="https://picsum.photos/200"
         />
         <WhatsOnCircle
