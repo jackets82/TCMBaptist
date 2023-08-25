59bd12d76912b82f322d1f5a5258fbb37fd72265
Adam Coates <ad_coates@hotmail.com>
2023-08-25 21:44:14 +0100

    Experiment
    Former-commit-id: 2134ea2c3d53e6ca02d5dd56916b8b4c47a4ef9a

--- app/components/Front Page/whyICome/WhyICome.js
+++ app/components/Front Page/whyICome/WhyICome.js
@@ -8,9 +8,9 @@ function WhyICome() {
           height="600"
           src="https://www.youtube.com/embed/-pzgRezQXsk?si=fcNEJyL-XOz-asYo"
           title="YouTube video player"
-          frameborder="0"
+          frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
+          allowFullScreen
         ></iframe>
       </div>
     </div>
--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import { Inter } from "next/font/google";
 import Cards from "./components/Front Page/Cards";
 import Hero from "./components/Hero/Hero";
@@ -5,10 +7,18 @@ import Welcome from "./components/Welcome";
 import WhatsOn from "./components/Front Page/whatson/WhatsOn";
 import WhyICome from "./components/Front Page/whyICome/WhyICome";
 import JoinUsMain from "./components/Front Page/joinUs/JoinUsMain";
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  useEffect(() => {
+    // window.addEventListener("scroll", () => {
+    //   document.documentElement.dataset.scroll = window.scrollY;
+    // });
+    console.log("Hello");
+  }, []);
+
   return (
     <main className="flex-grow">
       <Hero />
