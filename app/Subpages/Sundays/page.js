a04084be2533429c5a1e44b9d4924ccd020513a8
Adam Coates <ad_coates@hotmail.com>
2023-09-10 18:02:24 +0100

    Sunday details

--- app/Subpages/Sundays/page.js
+++ app/Subpages/Sundays/page.js
@@ -1,7 +1,15 @@
+import SubpageHeader from "../SubpageHeader";
+import Evenings from "./Evenings";
+import Mornings from "./Mornings";
+
 function page() {
   return (
     <div>
-      <p>This is the sunday deets</p>
+      <SubpageHeader title="Services" />
+      <div className="max-w-7xl mx-auto gap-20 flex flex-col bg-stone-200 p-10">
+        <Mornings />
+        <Evenings />
+      </div>
     </div>
   );
 }
