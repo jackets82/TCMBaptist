4e1885cfa6ffafea88aa662b5486eb1757d6ecd0
Adam Coates <ad_coates@hotmail.com>
2023-09-24 06:46:48 +0100

    joinuscards edited

--- app/components/Front Page/joinUs/JoinUsCards.js
+++ app/components/Front Page/joinUs/JoinUsCards.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 function JoinUsCards({ title, link, image = "/cross.jpg" }) {
   return (
     <button className=" rounded-2xl w-full h-52 relative overflow-hidden bg-red-600 hover:cursor-pointer">
-      <Link href={link} scroll={false}>
+      <Link href={link} scroll={true}>
         <Image src={image} fill style={{ objectFit: "cover" }} />{" "}
         <div className="w-full h-5 absolute bottom-5 text-center">
           <p className=" text-xl font-bold tracking-wider text-white">
