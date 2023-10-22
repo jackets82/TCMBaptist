5b6d219c6dc6385e65cbeeb7529b66eb601abc69
Adam Coates <ad_coates@hotmail.com>
2023-10-22 08:14:00 +0100

    another attempt at escaping characters

--- app/components/Home Page/Extra Stuff/DuringTheWeek.js
+++ app/components/Home Page/Extra Stuff/DuringTheWeek.js
@@ -5,14 +5,14 @@ function DuringTheWeek() {
     <div className="bg-orange-600">
       <div className="max-w-5xl mx-auto">
         <p className=" text-center py-10">
-          As well as our regular Sunday services, we also have youth work, men's
-          and women's ministries and much more
+          As well as our regular Sunday services, we also have youth work,
+          men&apos;s and women&apos;s ministries and much more
         </p>
         <div className="grid grid-cols-3 place-items-center gap-10">
           <ExtraStuffTile title="Extra Time" />
           <ExtraStuffTile title="House Groups" />
-          <ExtraStuffTile title="Mens Ministries" />
-          <ExtraStuffTile title="Womens Ministries" />
+          <ExtraStuffTile title="Men's Ministries" />
+          <ExtraStuffTile title="Women's Ministries" />
           <ExtraStuffTile title="Young Adults" />
           <ExtraStuffTile title="Children & Youth Groups" />
           <ExtraStuffTile title="Sunday School" />
